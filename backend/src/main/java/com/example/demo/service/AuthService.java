bc4600715e72a948942e570b126271b3f6177235
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-06-30 20:54:30 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.dto.AuthResponseDto;
 import com.example.demo.dto.LoginRequestDto;
 import com.example.demo.dto.RegisterPatientDto;
-
+@Service
 public interface AuthService {
     AuthResponseDto registerPatient(RegisterPatientDto dto);
     AuthResponseDto login(LoginRequestDto dto);

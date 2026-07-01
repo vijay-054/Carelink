027573d35d72b8db7d457a88dfd0e2769324df0b
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-07-01 09:28:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/BackendApplication.java
+++ backend/src/main/java/com/example/demo/BackendApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
+	// HelloVijay
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}

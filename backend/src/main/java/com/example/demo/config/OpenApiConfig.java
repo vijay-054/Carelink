c4314e77646914ba6b2c4674ad2c227be209590e
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-07-25 10:06:01 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/OpenApiConfig.java
+++ backend/src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";

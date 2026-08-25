d8df7eca49354b61312e55c27d7a78fae8b64d59
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-25 10:11:10 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import appointmentReducer from './slices/appointmentSlice';
-import doctorReducer from './slices/doctorSlice';
-import scheduleReducer from './slices/scheduleSlice';
-import patientReducer from './slices/patientSlice';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import authReducer from './store/slices/authSlice';
+import appointmentReducer from './store/slices/appointmentSlice';
+import doctorReducer from './store/slices/doctorSlice';
+import scheduleReducer from './store/slices/scheduleSlice';
+import patientReducer from './store/slices/patientSlice';
 
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        appointments: appointmentReducer,
-        doctors: doctorReducer,
-        schedule: scheduleReducer,
-        patients: patientReducer,
-    },
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);

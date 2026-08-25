fcda0ce8be74d88636270225384b299b23f09589
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-25 10:14:11 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import authReducer from './store/slices/authSlice';
-import appointmentReducer from './store/slices/appointmentSlice';
-import doctorReducer from './store/slices/doctorSlice';
-import scheduleReducer from './store/slices/scheduleSlice';
-import patientReducer from './store/slices/patientSlice';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

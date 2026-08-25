8596819a335e0d4a3ade267563d56aada987cde0
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-25 09:39:49 +0530

    User commit

--- frontend/src/__mocks__/components/store/slices/authSlice.js
+++ frontend/src/__mocks__/components/store/slices/authSlice.js
@@ -11,6 +11,24 @@ const initialState = {
     message: '',
 };
 
+// Register user
+export const register = createAsyncThunk(
+    'auth/register',
+    async (user, thunkAPI) => {
+        try {
+            return await authService.register(user);
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.error) ||
+                error.message ||
+                error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
 // Login user
 export const login = createAsyncThunk(
     'auth/login',
@@ -46,6 +64,22 @@ export const authSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
+            // Register Cases
+            .addCase(register.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.user = action.payload;
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                state.user = null;
+            })
+            // Login Cases
             .addCase(login.pending, (state) => {
                 state.isLoading = true;
             })

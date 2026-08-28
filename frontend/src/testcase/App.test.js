30d05bd83de2c4c4cb3c1386afb45991516c7f7b
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-28 11:47:38 +0530

    User commit

--- frontend/src/testcase/App.test.js
+++ frontend/src/testcase/App.test.js
@@ -1,9 +1,26 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from '../App';
+
+// test('renders dashboard header', () => {
+//   render(<App />);
+//   const headerElement = screen.getByText(/Hospital Appointment Management System/i);
+//   expect(headerElement).toBeInTheDocument();
+// });
+// //
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../store';
 import App from '../App';
 
 test('renders dashboard header', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const headerElement = screen.getByText(/Hospital Appointment Management System/i);
   expect(headerElement).toBeInTheDocument();
 });

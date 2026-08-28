97e48a5d95c5a8cee4ccf69facb13aea26fbaced
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-28 12:04:08 +0530

    User commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: 'http://localhost:8080/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+API.interceptors.request.use((config) => {
+    const userStr = localStorage.getItem('user');
+    if (userStr) {
+        try {
+            const user = JSON.parse(userStr);
+            if (user && user.token) {
+                config.headers.Authorization = `Bearer ${user.token}`;
+            }
+        } catch (e) {
+            console.error('Failed to parse user from localStorage', e);
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('user');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default API;

525405800db46abe5b19e09136fd4a2a96acd9ca
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-28 11:19:08 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import appointmentReducer from './slices/appointmentSlice';
+import doctorReducer from './slices/doctorSlice';
+import scheduleReducer from './slices/scheduleSlice';
+import patientReducer from './slices/patientSlice';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        appointments: appointmentReducer,
+        doctors: doctorReducer,
+        schedule: scheduleReducer,
+        patients: patientReducer,
+    },
+});

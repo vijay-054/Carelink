68ce11fb16dee013b8d5b0634cb0cfa479d06619
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-28 11:50:39 +0530

    User commit

---
+++ frontend/src/services/appointmentService.js
@@ -0,0 +1,7 @@
+const appointmentService = {
+  getMyAppointments: async () => [],
+  bookAppointment: async (data) => data,
+  cancelAppointment: async (id) => ({ id }),
+  getAllAppointments: async () => [],
+};
+export default appointmentService;

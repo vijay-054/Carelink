6fe27749b355cb13f8f4ebc20f9a767f1ab32169
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-08-29 13:56:05 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
 } from "react-router-dom";
 
-import Navbar from "./components/layout/Navbar";
+import Navbar from "./components/layout/navbar";
 
 import Login from "./components/Login";
 import Register from "./components/Register";

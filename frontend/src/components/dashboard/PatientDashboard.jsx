fa5cdeed4e0f3111d80ceac69e5db0916eecf92d
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-09-13 18:28:25 +0530

    User commit

--- frontend/src/components/dashboard/PatientDashboard.jsx
+++ frontend/src/components/dashboard/PatientDashboard.jsx
@@ -1,4 +1,4 @@
-import "./Dashboard.css";
+import "../dashboard/Dashboard.css";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";

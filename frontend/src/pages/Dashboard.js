8b67ea6199c42fdfff78d7960da2a9449f64a5db
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-09-15 09:28:53 +0530

    User commit

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,7 +2,11 @@ import React from "react";
 import PatientDashboard from "../components/dashboard/PatientDashboard";
 
 const Dashboard = () => {
-  return <PatientDashboard />;
+  return (
+    <div className="patient-dashboard-page">
+      <PatientDashboard />
+    </div>
+  );
 };
 
 export default Dashboard;

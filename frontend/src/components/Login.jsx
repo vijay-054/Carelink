3b4eadee7e1f0c85532517a1745b8165f0d821e4
VIJAY RAJ V <727824tuam054@skct.edu.in>
2026-09-15 08:39:46 +0530

    User commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import "./Login.css";
 
 import {
   login,
